0d42e1b91eaf17bfa70af881c99fa5767e27aa5d
Dipali Parab <dipaliparab182@gmail.com>
2024-06-12 11:15:24 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -9,19 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-// ********** set date ************
-// select span
-const date = (document.getElementById('date').innerHTML =
-  new Date().getFullYear())
-
-// ********** nav toggle ************
-// select button and links
-const navBtn = document.getElementById('nav-toggle')
-const links = document.getElementById('nav-links')
-// add event listener
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
-
-// ********** smooth scroll ************
-// select links
